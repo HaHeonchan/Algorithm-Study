19af7d8622fafae459ab29843ca4527b943d7e9f
heonchan <gjscks0106@gmail.com>
2024-03-31 16:42:51 +0900

    test
    파일 연동 테스트

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,26 +1,30 @@
 import sys
-n = int(sys.stdin.readline())
+stack = []
+com = int(sys.stdin.readline())
 
-stack=[]
-for i in range(n):
-    command = sys.stdin.readline().split()
-
-    if command[0]=='push':
-        stack.append(command[1])
-    elif command[0]=='pop':
-        if len(stack)==0:
+for i in range(com):
+    inputs = sys.stdin.readline().split()
+    
+    if inputs[0] == "push":
+        stack.append(inputs[1])
+        
+    elif inputs[0] == "pop":
+        if len(stack) == 0:
             print(-1)
         else:
             print(stack.pop())
-    elif command[0] == 'size':
+            
+    elif inputs[0] == "size":
         print(len(stack))
-    elif command[0] == 'empty':
-        if len(stack)==0:
+        
+    elif inputs[0] == "empty":
+        if len(stack) == 0:
             print(1)
         else:
             print(0)
-    elif command[0] == 'top':
-        if len(stack)==0:
+            
+    elif inputs[0] == "top":
+        if len(stack) == 0:
             print(-1)
         else:
             print(stack[-1])
