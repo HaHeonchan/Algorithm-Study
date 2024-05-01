7b908748871005ec6d3c330eb30632a49bb4719e
하헌찬 <127941120+HaHeonchan@users.noreply.github.com>
2024-05-01 14:38:21 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	string s;
+	getline(cin, s);
+
+	for (int i = 0; i <= s.length(); i++) {
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+		else if(s[i] >= 'A' && s[i] <= 'Z') {
+			s[i] += 32;
+		}
+	}
+	cout << s << endl;
+	return 0;
+}
