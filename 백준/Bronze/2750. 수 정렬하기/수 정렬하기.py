324fe5e99392a55e25425e1166bf1666219aa916
하헌찬 <127941120+HaHeonchan@users.noreply.github.com>
2025-10-12 21:17:41 +0900

    [Bronze II] Title: 수 정렬하기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,47 +1,43 @@
 import sys
 
-def mergesort(low, high):
-    if(low < high):
-        mid = int((low + high)/2)
-        mergesort(low, mid)
-        mergesort(mid+1, high)
-        merge(low, mid, high)
+def mergeSort(l, h):
+    if(l < h):
+        m = int((h + l)/2)
+        mergeSort(l, m)
+        mergeSort(m + 1, h)
+        merge(l, m, h)
 
-def merge(low, mid, high):
-    U = [0] * (high - low + 1)
-    i = low
-    j = mid + 1
-    k = low
+def merge(l, m, h):
+    m_arr = [0] * (h - l + 1)
+    i = l
+    k = l
+    j = m + 1
 
-    while i <= mid and j <= high:
-        if arr[i] < arr[j]:
-            U[k - low] = arr[i]
+    while(i <= m and j <= h):
+        if(arr[i] > arr[j]):
+            m_arr[k-l] = arr[i]
             i += 1
         else:
-            U[k - low] = arr[j]
+            m_arr[k-l] = arr[j]
             j += 1
         k += 1
     
-    if i > mid:
-        U[k - low:high - low + 1] = arr[j:high + 1]
+    if(i > m):
+        m_arr[k - l : h - l + 1] = arr[j : h + 1]
     else:
-        U[k - low:high - low + 1] = arr[i:mid + 1]
-    
-    arr[low:high + 1] = U
+        m_arr[k-l : h - l + 1] = arr[i : m + 1]
 
+    arr[l:h + 1] = m_arr
+        
 
 N = int(sys.stdin.readline())
 arr = []
 
 for i in range(N):
-    OwO = int(sys.stdin.readline())
-    arr.append(OwO)
+    n = int(sys.stdin.readline())
+    arr.append(n)
 
-mergesort(0, N-1)
-for i in range(N):
+mergeSort(0, len(arr)-1)
+
+for i in range(len(arr)-1, -1, -1):
     print(arr[i])
-
-
-#본래O(n^2)로 풀이해도 가능하지만
-#이번에는 특별히 배운것을 응용하기 위해 O(nlgn)으로 풀었다
-#하면서 후회는 되었지만 도움이 된 것 같다
