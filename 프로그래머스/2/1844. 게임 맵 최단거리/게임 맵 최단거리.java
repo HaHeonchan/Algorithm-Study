504cf577ffdf5bd8d577ec624b0f8331c85f7c16
하헌찬 <127941120+HaHeonchan@users.noreply.github.com>
2026-05-13 15:45:05 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.31 ms, Memory: 57.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,33 +2,28 @@ import java.util.*;
 
 class Solution {
     public static int solution(int[][] maps) {
-        int answer = 0;
         int[] dirX = {1, 0, -1, 0};
         int[] dirY = {0, -1, 0, 1};
         int lenX = maps.length;
         int lenY = maps[0].length;
 
 
-        int[][] distance = new int[lenX][lenY];
-        distance[0][0] = 1;
-
         Deque<int[]> tripQue = new ArrayDeque<>();
-        tripQue.add(new int[]{0, 0});
+        tripQue.add(new int[]{0, 0, 1});
 
         while (!tripQue.isEmpty()){
             int[] cPos = tripQue.remove();
-            int cx = cPos[0], cy = cPos[1];
+            int cx = cPos[0], cy = cPos[1], disC = cPos[2];
+            maps[cx][cy] = 0;
 
             for(int i = 0; i < 4; i++){
                 int nx = cx + dirX[i], ny = cy + dirY[i];
-                if((nx >= 0 && nx < maps.length && ny >= 0 && ny < maps[0].length && maps[nx][ny] == 1) && distance[nx][ny] == 0){
-                    distance[nx][ny] = distance[cx][cy] + 1;
-
+                if((nx >= 0 && nx < maps.length && ny >= 0 && ny < maps[0].length && maps[nx][ny] == 1)){
                     if(nx == lenX-1 && ny == lenY-1){
-                        return distance[lenX-1][lenY-1];
+                        return disC + 1;
                     }
-
-                    tripQue.add(new int[]{nx, ny});
+                    maps[nx][ny] = 0;
+                    tripQue.add(new int[]{nx, ny, disC + 1});
                 }
             }
         }
